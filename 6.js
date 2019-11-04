bc6ed247111840a66dc377d6f748c1942a335721
Zain UL Abideen <zain.san.su.152@gmail.com>
2019-11-04 15:06:24 +0500

    2nd commit

--- 6.js
+++ 6.js
@@ -2,5 +2,17 @@ var v =  new Vue({
     el:"#our-app",
     data:{
 
+    },
+    methods:{
+        keypressfunction:function (event) {
+            console.log(event.key);
+        },
+        keyupfunction:function () {
+            console.log("yes");
+        },
+        keydownfunction:function (event) {
+            console.log(event);
+        }
+
     }
 });
